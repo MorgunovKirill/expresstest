6e05a415add27f7de0ba45728802211fb0261d7c
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-07-22 18:28:54 +0400

    fix 2

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port =  process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    const HelloMessage = 'Hello incubator!!!!'
+    const HelloMessage = 'Hello safsdf!!!!'
     res.send(HelloMessage)
 })
 
